5a8eb6844869aa91c29459687a63f20d7d767dca
urskog84@github.com <karl.petter.andersson@gmail.com>
2018-08-07 20:34:52 +0200

    fix 2

--- start.ps1
+++ start.ps1
@@ -1,8 +1,19 @@
+
+Write-host "Rmove Coniainers"  -ForegroundColor Green
+docker-compose rm -f
+
 Write-Host "Start Bild of Contianers"  -ForegroundColor Green
 docker-compose build
 
-Write-Host "Start up cotiners" -ForegroundColor Green
+Write-Host "Starting cotiners" -ForegroundColor Green
 docker-compose up -d
+
+
+
+while ( !((Test-NetConnection localhost -Port 3000).TcpTestSucceeded)) {
+    Write-Host "Test connection to grafana continer" -ForegroundColor Green    
+}
+
 
 # Create Datasorces in Grafana
 Write-Host "Create Datasorces in Grafana" -ForegroundColor Yellow
